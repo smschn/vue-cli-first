df95b14c8266d0beacf5eb071f1e863b87ab58ee
Luca Somaschini <somas99@gmail.com>
2022-08-23 17:47:07 +0200

    Added component

---
+++ src/components/MyTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTitle'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
